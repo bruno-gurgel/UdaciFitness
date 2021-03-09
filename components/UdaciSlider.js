f7d6cc0965aac07ff169289ba565943ee83be635
Bruno Gurgel <bmg1612@gmail.com>
2021-03-09 15:48:13 -0300

    feat: Added slider

--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,10 +1,22 @@
+import Slider from "@react-native-community/slider";
 import React from "react";
 import { Text, View } from "react-native";
 
-export default function UdaciSlider() {
+export default function UdaciSlider(props) {
+  const { max, unit, step, value, onChange } = props;
   return (
     <View>
-      <Text>UdaciSlider</Text>
+      <Slider
+        step={step}
+        value={value}
+        maximumValue={max}
+        minimumValue={0}
+        onValueChange={onChange}
+      />
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
     </View>
-  )
+  );
 }

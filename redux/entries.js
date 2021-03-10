d201408642f1d44942f853ac00efb8c90df492d4
Bruno <bmg1612@gmail.com>
2021-03-10 16:28:46 -0300

    feat: Added slice

---
+++ redux/entries.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const entriesSlice = createSlice({
+	name: "entries",
+	initialState: {},
+	reducers: {
+		doReceiveEntries: (state, action) => (state = action.payload),
+		doAddEntry: (state, action) => state + action.payload,
+	},
+});
